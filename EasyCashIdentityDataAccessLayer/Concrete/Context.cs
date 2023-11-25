8e89b72cf0bc3411def76cc78e5c3d3bbbf2bf65
Baha Barut <20201129503@ogr.akdeniz.edu.tr>
2023-11-25 16:57:08 +0300

    Add project files.

---
+++ EasyCashIdentityDataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using EasyCashIdentityEntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityDataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-K9RNB1L;initial catalog=EasyCash;integrated security=true;");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
