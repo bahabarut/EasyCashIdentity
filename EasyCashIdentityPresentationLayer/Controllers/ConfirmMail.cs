00a75f0a1cad72826d381feafc75ce31ebb78774
Baha Barut <20201129503@ogr.akdeniz.edu.tr>
2023-12-10 15:01:13 +0300

    completed login process

--- EasyCashIdentityPresentationLayer/Controllers/ConfirmMail.cs
+++ EasyCashIdentityPresentationLayer/Controllers/ConfirmMail.cs
@@ -28,7 +28,7 @@ namespace EasyCashIdentityPresentationLayer.Controllers
             {
                 user.EmailConfirmed = true;
                 await _userManager.UpdateAsync(user);
-                return RedirectToAction("Index", "MyProfile");
+                return RedirectToAction("Index", "Login");
             }
             return View();
         }
---
+++ EasyCashIdentityPresentationLayer/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using EasyCashIdentityEntityLayer.Concrete;
+using EasyCashIdentityPresentationLayer.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityPresentationLayer.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public LoginController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel model)
+        {
+            // 4 parametre alır username, pass, şifremi hatırla alanı, db de Access Failed Count alanı 5 kere yanlış girerse kilitlenir hesap
+            var result = await _signInManager.PasswordSignInAsync(model.username, model.password, false, true);
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByNameAsync(model.username);
+                if (user.EmailConfirmed == true)
+                {
+
+                }
+                return RedirectToAction("Index", "");
+            }
+            return View();
+        }
+    }
+}
